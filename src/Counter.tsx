0b4d8f3fbfa25c973483b16de7d1f6fb9be74bdf
dev-93 <cr000@naver.com>
2021-01-27 02:13:10 +0900

    카운터 구현중

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { atom, selector } from 'recoil';
+import { atom, selector, useRecoilState } from 'recoil';
 import styled from 'styled-components';
-import CharacterCount from './CharacterCount';
-import TextInput from './TextInput';
 
 const textState = atom({
     key: 'textState', // unique ID (with respect to other atoms/selectors)
     default: '', // default value (aka initial value)
 });
 
+function TextInput() {
+    const [text, setText] = useRecoilState(textState);
+  
+    const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+      setText(event.target.value);
+    };
+  
+    return (
+      <div>
+        <input type="text" value={text} onChange={onChange} />
+        <br />
+        Echo: {text}
+      </div>
+    );
+}
+
 const Counter = () => {
     return(
         <Wrap>
             <TextInput />
-            <CharacterCount />
+            {/* <CharacterCount /> */}
         </Wrap>
     );
 };
