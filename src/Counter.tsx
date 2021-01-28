d72758069ad5ba1c8e1b0b3b3aeb6f65cca5c838
dev-93 <cr000@naver.com>
2021-01-28 23:55:58 +0900

    코드 정리

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,52 +3,52 @@ import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
 const textState = atom({
-    key: 'textState',
-    default: '',
+  key: 'textState',
+  default: '',
 });
 
 const charCountState = selector({
-    key: 'charCountState',
-    get: ({get}) => {
-      const text = get(textState);
-  
-      return text.length;
-    },
+  key: 'charCountState',
+  get: ({get}) => {
+    const text = get(textState);
+
+    return text.length;
+  },
 });
 
 function TextInput() {
-    const [text, setText] = useRecoilState(textState);
-  
-    const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {
-      setText(event.target.value);
-    };
-  
-    return (
-      <div>
-        <input type="text" value={text} onChange={onChange} />
-        <br />
-        Echo: {text}
-      </div>
-    );
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
 };
 
 function CharacterCount() {
-    const count = useRecoilValue(charCountState);
-  
-    return <>Character Count: {count}</>;
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
 };
 
 const Counter = () => {
-    return(
-        <Wrap>
-            <TextInput />
-            <CharacterCount />
-        </Wrap>
-    );
+  return(
+    <Wrap>
+      <TextInput />
+      <CharacterCount />
+    </Wrap>
+  );
 };
 
 const Wrap = styled.div`
-    border: 1px solid green;
+  border: 1px solid green;
 `;
 
 export default Counter;
